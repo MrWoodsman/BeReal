c31c6cfa7edc76aca2e9cbdf0daf24f47a7de5c0
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2022-10-06 04:15:15 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 var front = false;
-var video = document.querySelector('video');
+var cameraView = document.querySelector('video');
 // document.getElementById('flipCamera').onclick = function() { front = !front; };
-var constraints = { video: { facingMode: (front? "user" : "environment"), width: 5000, height: 5000  } };
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(mediaStream) {
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = function(e) {
-    video.play();
-};
-})
-.catch(function(err) { console.log(err.name + ": " + err.message); })
+// var constraints = { video: { facingMode: (front? "user" : "environment"), width: 1080, height: 1440  } };
+var constraints = { video: { facingMode: (front? "user" : "environment")} };
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+window.addEventListener("load", cameraStart, false);
 
 let canvas = document.getElementById('view')
 document.getElementById('MakePhoto').addEventListener('click', function() {
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    canvas.getContext('2d').drawImage(cameraView, 0, 0, canvas.width, canvas.height);
     let image_data_url = canvas.toDataURL('image/jpeg');
 
     // data url of the image
