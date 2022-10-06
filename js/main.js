9eda848a2e86f35c36bd6656a7b2b1576815ee58
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2022-10-06 03:37:57 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var front = false;
+var video = document.querySelector('video');
+// document.getElementById('flipCamera').onclick = function() { front = !front; };
+var constraints = { video: { facingMode: (front? "user" : "environment"), width: 640, height: 480  } };
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+    video.play();
+};
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); })
+
+let canvas = document.getElementById('view')
+document.getElementById('MakePhoto').addEventListener('click', function() {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    let image_data_url = canvas.toDataURL('image/jpeg');
+
+    // data url of the image
+    console.log(image_data_url);
+});
